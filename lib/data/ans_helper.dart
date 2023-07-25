076d17788eb9c655ea6f8207076d02eef74d0747
Svechka <34006663+Svechka@users.noreply.github.com>
2023-07-25 16:18:11 +0300

    mainthings

---
+++ lib/data/ans_helper.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class AHelper {
+  Future<String> getAnswer() async {
+    const fullUri = 'https://eightballapi.com/api';
+    final uri = Uri.parse(fullUri);
+    try {
+      final response = await http.get(uri);
+      final result = jsonDecode(response.body);
+      return result['reading'];
+    } catch (error) {
+      return '$error';
+    }
+  }
+}
--- lib/screen/magic_ball_screen.dart
+++ lib/screen/magic_ball_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:surf_practice_magic_ball/widgets/magic_ball.dart';
+import 'package:get/get.dart';
 
+// ignore: must_be_immutable
 class MagicBallScreen extends StatelessWidget {
-  const MagicBallScreen({Key? key}) : super(key: key);
+  MagicBallScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final String hintText =
+        'Нажмите на шар${GetPlatform.isDesktop || GetPlatform.isWeb ? '' : (' или потрясите ${MediaQuery.of(context).size.shortestSide < 550 ? 'телефон' : 'планшет'}')}';
+    return Scaffold(
+        body: Container(
+      padding: const EdgeInsets.only(top: 200),
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [
+              Colors.black,
+              Color.fromARGB(255, 4, 4, 70),
+              Color.fromARGB(255, 46, 2, 52),
+              Color.fromARGB(255, 74, 4, 83),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.1, 0.3, 0.8, 0.95]),
+      ),
+      child: Column(children: [
+        MagicBall(),
+        Text(
+          hintText,
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+          ),
+        )
+      ]),
+    ));
   }
 }
