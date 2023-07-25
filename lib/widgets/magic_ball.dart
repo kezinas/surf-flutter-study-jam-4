4057fbacb0af8857b806f1bc2a0092ff207c2f99
Svechka <34006663+Svechka@users.noreply.github.com>
2023-07-25 20:19:41 +0300

    little animation

--- lib/widgets/magic_ball.dart
+++ lib/widgets/magic_ball.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shake/shake.dart';
 import 'package:get/get.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:simple_animations/simple_animations.dart';
 import 'package:surf_practice_magic_ball/data/ans_helper.dart';
 
 class MagicBall extends StatefulWidget {
@@ -11,15 +12,18 @@ class MagicBall extends StatefulWidget {
   State<StatefulWidget> createState() => _MagicBallState();
 }
 
-class _MagicBallState extends State<MagicBall> {
+class _MagicBallState extends State<MagicBall>
+    with SingleTickerProviderStateMixin {
   String answer = '';
   AHelper helper = AHelper();
   bool onTapped = false;
   bool hasError = false;
+  late AnimationController animationController;
 
   @override
   void initState() {
     super.initState();
+    animationController = AnimationController(vsync: this);
     if (!(GetPlatform.isDesktop || GetPlatform.isWeb)) {
       ShakeDetector.autoStart(
           shakeThresholdGravity: 1.5,
@@ -52,64 +56,78 @@ class _MagicBallState extends State<MagicBall> {
         }
         setState(() {});
       },
-      child: Stack(
-        alignment: AlignmentDirectional.center,
-        textDirection: TextDirection.ltr,
-        children: [
-          Container(
-            width: 300,
-            height: 300,
-            margin: const EdgeInsets.only(bottom: 40),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(300),
-              border: Border.all(),
-              image: DecorationImage(
-                image: AssetImage('assets/images/ball_sc.jpg'),
-                fit: BoxFit.fitHeight,
+      child: LoopAnimationBuilder(
+        duration: Duration(seconds: 3),
+        tween: Tween(begin: 10.0, end: 0.0),
+        builder: (BuildContext context, value, _) {
+          return Stack(
+            alignment: AlignmentDirectional.center,
+            textDirection: TextDirection.ltr,
+            children: [
+              Container(
+                width: 300,
+                height: 300,
+                margin: EdgeInsets.only(bottom: 40.0 - value, top: value),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(300),
+                  border: Border.all(),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/ball_sc.jpg'),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
               ),
-            ),
-          ),
-          Container(
-            width: 300,
-            height: 300,
-            margin: const EdgeInsets.only(bottom: 40),
-            padding: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(300),
-            ),
-            child: Center(child: SvgPicture.asset('assets/images/star.svg')),
-          ),
-          Container(
-            width: 300,
-            height: 300,
-            margin: const EdgeInsets.only(bottom: 40),
-            padding: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(300),
-            ),
-            child: Center(
-                child: SvgPicture.asset(
-              'assets/images/small star.svg',
-            )),
-          ),
-          onTapped
-              ? Container(
-                  width: 300,
-                  height: 300,
-                  margin: const EdgeInsets.only(bottom: 40),
-                  padding: EdgeInsets.all(30),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(300),
-                      color: hasError
-                          ? Color.fromARGB(135, 250, 3, 3)
-                          : Color.fromARGB(136, 0, 0, 0)),
-                )
-              : Container(),
-          Text(
-            answer,
-            style: TextStyle(color: Colors.white, fontSize: 22),
-          ),
-        ],
+              Container(
+                width: 300,
+                height: 300,
+                margin: EdgeInsets.only(bottom: 40.0 - value, top: value),
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(300),
+                ),
+                child:
+                    Center(child: SvgPicture.asset('assets/images/star.svg')),
+              ),
+              Container(
+                width: 300,
+                height: 300,
+                margin: EdgeInsets.only(bottom: 40.0 - value, top: value),
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(300),
+                ),
+                child: Center(
+                    child: SvgPicture.asset(
+                  'assets/images/small star.svg',
+                )),
+              ),
+              onTapped
+                  ? Container(
+                      width: 300,
+                      height: 300,
+                      margin: EdgeInsets.only(bottom: 40.0 - value, top: value),
+                      padding: EdgeInsets.all(30),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(300),
+                          color: hasError
+                              ? Color.fromARGB(135, 250, 3, 3)
+                              : Color.fromARGB(136, 0, 0, 0)),
+                    )
+                  : Container(),
+              Container(
+                width: 300,
+                height: 300,
+                margin: EdgeInsets.only(bottom: 40.0 - value, top: value),
+                padding: EdgeInsets.all(30),
+                alignment: Alignment.center,
+                child: Text(
+                  answer,
+                  style: TextStyle(color: Colors.white, fontSize: 22),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
